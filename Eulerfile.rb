0f23cd5a791058fbc2f8dddbee11b97f1095dd4b
Phil Pirozhkov <pirjsuka@gmail.com>
2015-11-10 17:37:47 +0300

    ease working on new problems

---
+++ Eulerfile.rb
@@ -0,0 +1,6 @@
+Euler.config do |config|
+  # Images included in problem prompts are typically loaded from your local
+  # file system.  You can opt into loading the images from this gem's github
+  # repo by uncommenting the following line.
+  config.images_dir "https://raw.githubusercontent.com/yaworsw/euler-manager/develop/data/images"
+end
---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+ruby '2.2.3'
+
+gem 'euler-manager'
+gem 'benchmark-ips'
